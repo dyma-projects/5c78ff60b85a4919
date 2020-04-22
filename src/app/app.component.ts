dc08b2ab4d7d708400a263e120c44e93f76ddf45
Aina <mplurv@gmail.com>
2020-04-22 19:19:13 +0200

    task 4 - un clic sur le composant exercice2 doit afficher clic dans la console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projet2';
+
+  public clickComponent2(): void {
+    console.log('clic');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Exercice1Component } from './exercice1/exercice1.component';
+import { Exercice2Component } from './exercice2/exercice2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Exercice1Component
+    Exercice1Component,
+    Exercice2Component
   ],
   imports: [
     BrowserModule
