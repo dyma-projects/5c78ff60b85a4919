a33d98a35e6f4473d00edd7d98e17bf1ddb7d163
Aina <mplurv@gmail.com>
2020-04-22 19:16:41 +0200

    task 3 - dans ce composant mettez en place trois exemple de liaison

--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-exercice1',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice1Component implements OnInit {
 
+  public text: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.text = 'exercice1 works!';
+  }
+
+  public clickEvent(event): void {
+    console.log(`exercice1 clicked`);
   }
 
 }
